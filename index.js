52eb3eefd9963c00a23361d9fdd0b0b29e79a0b9
paolodo <pa.dorsi@gmail.com>
2019-09-18 11:14:10 +0200

    test 2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var website = require("./setup");
 var postcss = require("metalsmith-postcss");
 var htmlMinifier = require("metalsmith-html-minifier");
+var serve = require('metalsmith-serve');
+var watch = require('metalsmith-watch');
 
 website
   .use(
@@ -12,6 +14,16 @@ website
     })
   )
   .use(htmlMinifier())
+.use(serve({
+  port: 8081,
+  verbose: true
+}))
+.use(watch({
+    paths: {
+      "${source}/**/*": true,
+      "layout/**/*": "**/*",
+    }
+  }))
   .build(function(err) {
     // build process
     if (err) throw err; // error handling is required
